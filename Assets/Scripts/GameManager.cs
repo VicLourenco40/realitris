37ac1220ecaeda92c020694d210dde1a4931310d
Vicente Lourenço <134103080+VicLourenco40@users.noreply.github.com>
2024-05-15 14:37:53 +0100

    Add piece shapes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,156 @@ public class GameManager : MonoBehaviour
     private const int GridExtraRows = 4;
     private const int GridColumns = 10;
 
-    private bool[,] grid = new bool[GridRows + GridExtraRows, GridColumns];
+    private readonly int[][,] IShape = {
+        new int[,] {
+            { 0, 0, 0, 0 },
+            { 1, 1, 1, 1 },
+            { 0, 0, 0, 0 },
+            { 0, 0, 0, 0 }
+        },
+        new int[,] {
+            { 0, 0, 1, 0 },
+            { 0, 0, 1, 0 },
+            { 0, 0, 1, 0 },
+            { 0, 0, 1, 0 }
+        },
+        new int[,] {
+            { 0, 0, 0, 0 },
+            { 0, 0, 0, 0 },
+            { 1, 1, 1, 1 },
+            { 0, 0, 0, 0 }
+        },
+        new int[,] {
+            { 0, 1, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 1, 0, 0 }
+        }
+    };
+
+    private readonly int[][,] JShape = {
+        new int[,] {
+            { 1, 0, 0 },
+            { 1, 1, 1 },
+            { 0, 0, 0 }
+        },
+        new int[,] {
+            { 0, 1, 1 },
+            { 0, 1, 0 },
+            { 0, 1, 0 }
+        },
+        new int[,] {
+            { 0, 0, 0 },
+            { 1, 1, 1 },
+            { 0, 0, 1 }
+        },
+        new int[,] {
+            { 0, 1, 0 },
+            { 0, 1, 0 },
+            { 1, 1, 0 }
+        }
+    };
+
+    private readonly int[][,] LShape = {
+        new int[,] {
+            { 0, 0, 1 },
+            { 1, 1, 1 },
+            { 0, 0, 0 }
+        },
+        new int[,] {
+            { 0, 1, 0 },
+            { 0, 1, 0 },
+            { 0, 1, 1 }
+        },
+        new int[,] {
+            { 0, 0, 0 },
+            { 1, 1, 1 },
+            { 1, 0, 0 }
+        },
+        new int[,] {
+            { 1, 1, 0 },
+            { 0, 1, 0 },
+            { 0, 1, 0 }
+        }
+    };
+
+    private readonly int[][,] SShape = {
+        new int[,] {
+            { 0, 1, 1 },
+            { 1, 1, 0 },
+            { 0, 0, 0 }
+        },
+        new int[,] {
+            { 0, 1, 0 },
+            { 0, 1, 1 },
+            { 0, 0, 1 }
+        },
+        new int[,] {
+            { 0, 0, 0 },
+            { 0, 1, 1 },
+            { 1, 1, 0 }
+        },
+        new int[,] {
+            { 1, 0, 0 },
+            { 1, 1, 0 },
+            { 0, 1, 0 }
+        }
+    };
+
+    private readonly int[][,] ZShape = {
+        new int[,] {
+            { 1, 1, 0 },
+            { 0, 1, 1 },
+            { 0, 0, 0 }
+        },
+        new int[,] {
+            { 0, 0, 1 },
+            { 0, 1, 1 },
+            { 0, 1, 0 }
+        },
+        new int[,] {
+            { 0, 0, 0 },
+            { 1, 1, 0 },
+            { 0, 1, 1 }
+        },
+        new int[,] {
+            { 0, 1, 0 },
+            { 1, 1, 0 },
+            { 1, 0, 0 }
+        }
+    };
+
+    private readonly int[][,] TShape = {
+        new int[,] {
+            { 0, 1, 0 },
+            { 1, 1, 1 },
+            { 0, 0, 0 }
+        },
+        new int[,] {
+            { 0, 1, 0 },
+            { 0, 1, 1 },
+            { 0, 1, 0 }
+        },
+        new int[,] {
+            { 0, 0, 0 },
+            { 1, 1, 1 },
+            { 0, 1, 0 }
+        },
+        new int[,] {
+            { 0, 1, 0 },
+            { 1, 1, 0 },
+            { 0, 1, 0 }
+        }
+    };
+
+    private readonly int[][,] OShape = {
+        new int[,] {
+            { 1, 1 },
+            { 1, 1 },
+        }
+    };
+
+    private int[,] grid = new int[GridRows + GridExtraRows, GridColumns];
 
     void Start() { }
 
@@ -22,8 +171,8 @@ public class GameManager : MonoBehaviour
 
         for (int row = GridExtraRows; row < GridRows + GridExtraRows; row++) {
             for (int column = 0; column < GridColumns; column++) {
-                grid[row, column] = true;
-                displayText += grid[row, column] ? "# " : "  ";
+                grid[row, column] = 1; // TEMP
+                displayText += grid[row, column] == 1 ? "# " : "  ";
             }
 
             displayText += "\n";
